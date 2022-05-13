90ddd1c8002b31c10caa8d50ddb6454b61453118
Christian Slothouber <f.c.slothouber@gmail.com>
2022-05-13 23:08:02 +0200

    Add contrived pokemon example

---
+++ Faab.Crops.Test/UseCaseExampleTests.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+
+namespace Faab.Crops.Test;
+
+[TestFixture]
+public class UseCaseExampleTests
+{
+    private static Crop<int> GetFirstGenerationPokemonStrength(int id)
+    {
+        return id switch
+        {
+            <= 0 => CropStatus.BadRequest,
+            > 151 => CropStatus.NotFound,
+            6 => 100,
+            _ => 0
+        };
+    }
+
+    [Test]
+    public void ContrivedUseCase()
+    {
+        // Act
+        var missingno = GetFirstGenerationPokemonStrength(0);
+        var absol = GetFirstGenerationPokemonStrength(359);
+        var charizard = GetFirstGenerationPokemonStrength(6);
+        var pikachu = GetFirstGenerationPokemonStrength(25);
+
+        // Assert
+        Assert.AreEqual(CropStatus.BadRequest, missingno.Status);
+        Assert.AreEqual(0, missingno.Value);
+        
+        Assert.AreEqual(CropStatus.NotFound, absol.Status);
+        Assert.AreEqual(0, absol.Value);
+        
+        Assert.AreEqual(CropStatus.Ok, charizard.Status);
+        Assert.AreEqual(100, charizard.Value);
+        
+        Assert.AreEqual(CropStatus.Ok, pikachu.Status);
+        Assert.AreEqual(0, pikachu.Value);
+    }
+}
